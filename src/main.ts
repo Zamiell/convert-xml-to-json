410d42552d987cc9cd5206a717491d15026b60ac
James <5511220+Zamiell@users.noreply.github.com>
2023-02-05 13:40:09 -0500

    feat: path normalization

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
 
 import chalk from "chalk";
-import * as path from "node:path";
 import sourceMapSupport from "source-map-support";
 import xml2js from "xml2js";
 import { fileExists, isFile, readFile, writeFile } from "./file";
@@ -15,9 +14,6 @@ function main() {
 
   // Get command line arguments.
   const args = parseArgs();
-
-  args.xmlPath = path.resolve(args.xmlPath);
-  args.jsonPath = path.resolve(args.xmlPath);
 
   convertXMLToJson(args.xmlPath, args.jsonPath);
 }
